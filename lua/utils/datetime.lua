08aa4baa1095ddc05e6954eb4f087d7d5b69d227
sbxlmdsl <sbxlmdsl@qq.com>
2024-02-06 11:05:20 +0800

    improve format of datetime

--- lua/utils/datetime.lua
+++ lua/utils/datetime.lua
@@ -8,7 +8,7 @@ local function translator(input, seg)
    end
    if (input == "ors") then
       yield(Candidate("ors", seg.start, seg._end, os.date("%Y年%m月%d日%H时%M分%S秒"), ""))
-      yield(Candidate("ors", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
+      yield(Candidate("ors", seg.start, seg._end, os.date("%Y-%m-%d %H:%M:%S"), ""))
    end
    if (input == "osj") then
       yield(Candidate("osj", seg.start, seg._end, os.date("%H时%M分%S秒"), ""))
