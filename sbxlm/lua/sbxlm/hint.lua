eedbb0999bd204c8633a2cfc7af23f8ede5733ee
sbxlmdsl <sbxlmdsl@qq.com>
2024-03-27 17:33:12 +0800

    remove bug for sbfx in hint.lua

--- sbxlm/lua/sbxlm/hint.lua
+++ sbxlm/lua/sbxlm/hint.lua
@@ -159,7 +159,7 @@ function this.func(translation, env)
 		end
 		-- 简码、飞系和双拼在常规码位上，提示声声词和声声笔词，在增强模式下还提示数选字词
 		if (core.fm(id) or core.fd(id) or core.sp(id)) and rime.match(input, "([bpmfdtnlgkhjqxzcsrywv][a-z]){2}[aeuio]{0,2}")
-			or core.fx(id) and rime.match(input, "[bpmfdtnlgkhjqxzcsrywv][a-z][bpmfdtnlgkhjqxzcsrywv][aeuio]{0,2}")
+			or core.fx(id) and rime.match(input, "[bpmfdtnlgkhjqxzcsrywv][a-z][bpmfdtnlgkhjqxzcsrywv][0-9aeuio][aeuio]{0,3}")
 			or core.jm(id) and rime.match(input, "[bpmfdtnlgkhjqxzcsrywv][a-z]{3}[aeuio]{0,2}") then
 			local codes = env.reverse:lookup(candidate.text)
 			for code in string.gmatch(codes, "[^ ]+") do
