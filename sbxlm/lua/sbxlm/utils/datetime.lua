ae7b8f711792f12692c88072e06a6c667c54aeb9
sbxlmdsl <sbxlm@qq.com>
2024-06-25 11:38:04 +0800

    update datetime.lua

--- sbxlm/lua/sbxlm/utils/datetime.lua
+++ sbxlm/lua/sbxlm/utils/datetime.lua
@@ -9,12 +9,15 @@ local function translator(input, seg)
    if (input == "orq") then
       table.insert(datetimes, os.date("%Y年%m月%d日"))
       table.insert(datetimes, os.date("%Y-%m-%d"))
+      table.insert(datetimes, os.date("%Y%m%d"))
    elseif (input == "osj") then
       table.insert(datetimes, os.date("%H时%M分%S秒"))
       table.insert(datetimes, os.date("%H:%M:%S"))
+      table.insert(datetimes, os.date("%H%M%S"))
    elseif (input == "ors") then
       table.insert(datetimes, os.date("%Y年%m月%d日%H时%M分%S秒"))
       table.insert(datetimes, os.date("%Y-%m-%d %H:%M:%S"))
+      table.insert(datetimes, os.date("%Y%m%d%H%M%S"))
    end
    for _, entry in ipairs(datetimes) do
       ---@cast entry string
