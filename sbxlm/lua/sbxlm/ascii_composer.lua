e544f1377dab62ee536d3842528a9c2b957d6c8d
sbxlmdsl <sbxlm@qq.com>
2024-07-08 08:23:48 +0800

    fix bugs in ascii_composer.lua

--- sbxlm/lua/sbxlm/ascii_composer.lua
+++ sbxlm/lua/sbxlm/ascii_composer.lua
@@ -13,11 +13,13 @@ local this = {}
 
 ---@class AsciiComposerEnv: Env
 ---@field ascii_composer Processor
+---@field selector Processor
 ---@field connection Connection
 
 ---@param env AsciiComposerEnv
 function this.init(env)
   env.ascii_composer = rime.Processor(env.engine, "", "ascii_composer")
+  env.selector = rime.Processor(env.engine, "", "selector")
 end
 
 ---@param ch number
@@ -78,9 +80,12 @@ function this.func(key_event, env)
   -- 在码长为4以上时，设置临时重码提示
   elseif (not ascii_mode and segment and segment:has_tag("abc") and input:len() >= 4 and input:len() <= 5
       and key_event.keycode == XK_Tab and not key_event:release()) then
-        if context:get_option("single_display") and not context:get_option("not_single_display") and key_event.modifier ~= rime.modifier_masks.kShift then
+        if context:get_option("single_display") and not context:get_option("not_single_display") then
           context:set_option("not_single_display", true)
-          return rime.process_results.kAccepted
+          if  key_event.modifier ~= rime.modifier_masks.kShift then
+            env.selector:process_key_event(key_event)
+            return rime.process_results.kAccepted
+          end
         end
         return rime.process_results.kNoop
   -- 声笔简码在码长5以上时，单引号进入打空造词
