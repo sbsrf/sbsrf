fcc5ca17ea61ec2f16a52ff568061f2df695561d
sbxlmdsl <sbxlm@qq.com>
2024-12-07 17:12:26 +0800

    消除声笔大写状态切换输入法的BUG

--- sbxlm/lua/sbxlm/ascii_composer.lua
+++ sbxlm/lua/sbxlm/ascii_composer.lua
@@ -59,7 +59,7 @@ function this.func(key_event, env)
   local schema_id = env.engine.schema.schema_id
 
   -- auto_inline 启用时，首字母大写时自动切换到内联模式
-  if (not ascii_mode and auto_inline and input:len() == 0 and is_upper(key_event.keycode)) then
+  if not ascii_mode and auto_inline and input:len() == 0 and is_upper(key_event.keycode) and not key_event:caps() then
     if (key_event:shift() and key_event:ctrl()) or key_event:alt() or key_event:super() or key_event:release() then
       return rime.process_results.kNoop
     end
